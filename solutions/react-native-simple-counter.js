ca6df6aa003568c08981c7b1f299895fa3117ec3
Kayla Kremer <kaylakremer@icloud.com>
2023-01-08 04:56:16 -0500

    feat: add solution for react native version of simple counter challenge

---
+++ solutions/react-native-simple-counter.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react"
+import { Text, View, StyleSheet } from "react-native"
+
+const SimpleCounter = () => {
+    const [count, setCount] = useState(0)
+
+    const increment = () => setCount(count + 1)
+    return (
+        <View style={styles.container}>
+            <Text style={styles.counter}>
+                button count: <span id="actualCount">{count}</span>
+            </Text>
+            <button id="mainButton" onClick={increment}>
+                INCREASE
+            </button>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        marginHorizontal: 10,
+    },
+    counter: {
+        textAlign: "center",
+        marginVertical: 10,
+    },
+})
+
+export default SimpleCounter
