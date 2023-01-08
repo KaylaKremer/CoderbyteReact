dcae511a46dd355d0c5bdf7a3e5e0ae569427f0c
Kayla Kremer <kaylakremer@icloud.com>
2023-01-08 03:40:49 -0500

    feat: add solution for phone book challenge

---
+++ solutions/phone-book.js
@@ -0,0 +1,168 @@
+import React, { useState, useReducer } from "react"
+import ReactDOM from "react-dom"
+
+const style = {
+    table: {
+        borderCollapse: "collapse",
+    },
+    tableCell: {
+        border: "1px solid gray",
+        margin: 0,
+        padding: "5px 10px",
+        width: "max-content",
+        minWidth: "150px",
+    },
+    form: {
+        container: {
+            padding: "20px",
+            border: "1px solid #F0F8FF",
+            borderRadius: "15px",
+            width: "max-content",
+            marginBottom: "40px",
+        },
+        inputs: {
+            marginBottom: "5px",
+        },
+        submitBtn: {
+            marginTop: "10px",
+            padding: "10px 15px",
+            border: "none",
+            backgroundColor: "lightseagreen",
+            fontSize: "14px",
+            borderRadius: "5px",
+        },
+    },
+}
+
+function PhoneBookForm({ addEntryToPhoneBook }) {
+    const initialFormState = {
+        userFirstname: "Coder",
+        userLastname: "Byte",
+        userPhone: "8885559999",
+    }
+
+    const formReducer = (state, { type, payload }) => {
+        switch (type) {
+            case "RESET":
+                return { userFirstname: "", userLastname: "", userPhone: "" }
+            default:
+                return { ...state, [type]: payload }
+        }
+    }
+
+    const [formState, dispatch] = useReducer(formReducer, initialFormState)
+
+    return (
+        <form
+            onSubmit={e => {
+                e.preventDefault()
+                addEntryToPhoneBook(formState)
+                dispatch({ type: "RESET" })
+            }}
+            style={style.form.container}
+        >
+            <label>First name:</label>
+            <br />
+            <input
+                style={style.form.inputs}
+                className="userFirstname"
+                name="userFirstname"
+                type="text"
+                onChange={({ target: { name, value } }) =>
+                    dispatch({ type: name, payload: value })
+                }
+                value={formState.userFirstname}
+            />
+            <br />
+            <label>Last name:</label>
+            <br />
+            <input
+                style={style.form.inputs}
+                className="userLastname"
+                name="userLastname"
+                type="text"
+                onChange={({ target: { name, value } }) =>
+                    dispatch({ type: name, payload: value })
+                }
+                value={formState.userLastname}
+            />
+            <br />
+            <label>Phone:</label>
+            <br />
+            <input
+                style={style.form.inputs}
+                className="userPhone"
+                name="userPhone"
+                type="text"
+                onChange={({ target: { name, value } }) =>
+                    dispatch({ type: name, payload: value })
+                }
+                value={formState.userPhone}
+            />
+            <br />
+            <input
+                style={style.form.submitBtn}
+                className="submitButton"
+                type="submit"
+                value="Add User"
+            />
+        </form>
+    )
+}
+
+function InformationTable({ entries }) {
+    return (
+        <table style={style.table} className="informationTable">
+            <thead>
+                <tr key={`row-0`}>
+                    <th style={style.tableCell}>First name</th>
+                    <th style={style.tableCell}>Last name</th>
+                    <th style={style.tableCell}>Phone</th>
+                </tr>
+                {entries.map(
+                    ({ userFirstname, userLastname, userPhone }, index) => (
+                        <tr key={`row-${index + 1}`}>
+                            <td style={style.tableCell}>{userFirstname}</td>
+                            <td style={style.tableCell}>{userLastname}</td>
+                            <td style={style.tableCell}>{userPhone}</td>
+                        </tr>
+                    )
+                )}
+            </thead>
+        </table>
+    )
+}
+
+function Application() {
+    const [entries, setEntries] = useState([])
+
+    const addEntryToPhoneBook = ({
+        userFirstname,
+        userLastname,
+        userPhone,
+    }) => {
+        const newEntries = [
+            ...entries,
+            { userFirstname, userLastname, userPhone },
+        ]
+        const newSortedEntries = newEntries.sort((a, b) => {
+            const userLastnameA = a.userLastname.toLowerCase()
+            const userLastnameB = b.userLastname.toLowerCase()
+            return userLastnameA < userLastnameB
+                ? -1
+                : userLastnameA > userLastnameB
+                ? 1
+                : 0
+        })
+        setEntries(newSortedEntries)
+    }
+
+    return (
+        <section>
+            <PhoneBookForm addEntryToPhoneBook={addEntryToPhoneBook} />
+            <InformationTable entries={entries} />
+        </section>
+    )
+}
+
+ReactDOM.render(<Application />, document.getElementById("root"))
