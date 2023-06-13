82c16061452cbd0913825d8f846d0a3c31f179c6
Kayla Kremer <kkremer@getharvest.com>
2023-06-13 15:57:22 -0400

    refactor: function name and return value

--- solutions/typescript-button-toggle.ts
+++ solutions/typescript-button-toggle.ts
@@ -4,12 +4,12 @@ import ReactDOM from "react-dom"
 function Toggle(): ReactNode {
     const [toggle, setToggle] = useState<boolean>(true)
 
-    function handleClick() {
+    function onClick(): void {
         setToggle(!toggle)
     }
 
     return (
-        <button type="button" onClick={handleClick}>
+        <button type="button" onClick={onClick}>
             {toggle ? "ON" : "OFF"}
         </button>
     )
