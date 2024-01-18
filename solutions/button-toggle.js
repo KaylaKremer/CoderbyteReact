5fa3a960d946a3bc00874eafc3ab56d99b785f80
Kayla Kremer <kkremer@getharvest.com>
2024-01-18 15:14:31 -0500

    refactor: button-toggle.js

--- solutions/button-toggle.js
+++ solutions/button-toggle.js
@@ -1,22 +1,19 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react"
+import { createRoot } from "react-dom/client"
 
 const Toggle = () => {
+    const [toggle, setToggle] = useState(false)
 
-  const [toggle, setToggle] = useState(false);
-  
-  const handleClick = () => {
-    setToggle(!toggle);
-  };
+    const handleClick = () => {
+        setToggle(!toggle)
+    }
 
     return (
-      <button type="button" onClick={handleClick}>
-        {toggle ? 'ON' : 'OFF'}
-      </button>
-    );
-};
+        <button type="button" onClick={handleClick}>
+            {toggle ? "ON" : "OFF"}
+        </button>
+    )
+}
 
-ReactDOM.render(
-  <Toggle />,
-  document.getElementById('root')
-);
+const root = createRoot(document.getElementById("root"))
+root.render(<Toggle />)
